8ab6f7bdf66d7a992318f84d8414de9c94043229
Khoi HOANG <khoi@drivemode.com>
2023-07-15 18:09:14 +0900

    Initialized the `TaskBucketRepository`

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,5 +1,7 @@
+use crate::model::aggregate::TaskBucket;
 use async_trait::async_trait;
 
 #[async_trait]
 trait TaskBucketRepository {
+    async fn create_or_update(&self, task_bucket: &TaskBucket);
 }
