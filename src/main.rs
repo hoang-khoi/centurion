3e368f5f747b0b73707a7ab88e821f8fd863a7c2
Khoi HOANG <khoi@drivemode.com>
2023-07-19 21:08:25 +0900

    Added logging for main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::factory::{socket_addr, task_service_server};
+use log::info;
 use tonic::transport::Server;
 
 pub mod factory;
@@ -10,9 +11,12 @@ pub mod service;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
+    let addr = socket_addr();
+    info!("Serving: {}", addr);
+
     Server::builder()
         .add_service(task_service_server())
-        .serve(socket_addr())
+        .serve(addr)
         .await?;
 
     Ok(())
